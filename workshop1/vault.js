0316e4056765b54bee98b2ff5f32aba6043ef77b
Nathan Kim <nastynate@pop-os.localdomain>
2023-06-27 16:30:45 -0400

    initial commit for workshop1

--- workshop1/vault.js
+++ workshop1/vault.js
@@ -1,9 +1,16 @@
 // Combo: 10 - 40 - 39
 
-// Create variables
+
+//
+
+// Assign 3 variables
 let comb1 = 5 * 2;
 let comb2 = 4 * comb1;
 let comb3 = comb2 - 1;
 
-alert(`${comb1} - ${comb2} - ${comb3}`);
-    
+//create message
+let message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+// create a dialogue box displaying the vault codes and the text using the HTML tags and the string.
+alert(message +`${comb1} - ${comb2} - ${comb3}`);
+
