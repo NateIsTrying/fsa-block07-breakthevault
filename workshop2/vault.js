bf1f981dd27317718c5ba447315f96271cca16c8
Nathan Kim <nastynate@pop-os.localdomain>
2023-06-27 17:55:34 -0400

    added do-while loop to test, having issue finding solution

--- workshop2/vault.js
+++ workshop2/vault.js
@@ -1,9 +1,17 @@
 let yourName = prompt('Please enter your name.');
 
-let num1 = prompt('Give us your first number.');
-
+let num1 = parseInt(prompt('Give us your first number.'));
+// do {
+//     num1 = prompt('Please give us a NUMBER...');
+// } while(isNaN(num1) !== 'number');
+alert(typeof(num1))
 let num2 = prompt('Give us your second number.');
 
 let num3 = prompt('Give us your third number.');
 
 alert(`Thank you ${yourName}. Your combination is ${num1}-${num2}-${num3}.`);
+
+let sum = parseInt(num1) + parseInt(num2) + parseInt(num3);
+
+alert(`The sum of your numbers is ${sum}`);
+
